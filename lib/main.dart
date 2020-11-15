8a6400a8132d4e2efdb5443e82a46de7049460c9
Eser Alexander (s) <alexander.eser@students.fhnw.ch>
2020-11-15 11:49:29 +0100

    Fixed merge conflict

--- lib/main.dart
+++ lib/main.dart
@@ -4,27 +4,25 @@ import 'package:flutter/material.dart';
 import 'package:ip5_selbsteinschaetzung/database/database_initial_data.dart';
 import 'package:ip5_selbsteinschaetzung/database/database.dart';
 
-
-Future<void> main() async{
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   final callback = Callback(
-    //when initialized for the first time
-    onCreate: (database, version) async {
-      const initScript = initialDataScript;
-      for (final script in initScript) {
-        await database.execute(script);
-      }
-    },
-    //when app is opened
-    onOpen: (database) async{
-      //tbd
-    },
-    //when upgraded
-    onUpgrade: (database, startVersion, endVersion){
-      //tbd
+      //when initialized for the first time
+      onCreate: (database, version) async {
+    const initScript = initialDataScript;
+    for (final script in initScript) {
+      await database.execute(script);
     }
-  );
+  },
+      //when app is opened
+      onOpen: (database) async {
+    //tbd
+  },
+      //when upgraded
+      onUpgrade: (database, startVersion, endVersion) {
+    //tbd
+  });
 
   final database = await $FloorAppDatabase
       .databaseBuilder('database.db')
@@ -34,12 +32,10 @@ Future<void> main() async{
   runApp(MyApp(database));
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp(this.database);
 
   final AppDatabase database;
-
 
   @override
   Widget build(BuildContext context) {
@@ -61,17 +57,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        body: Center(
+      body: Center(
           child: Center(
-            child: Text("Hello World"),
-          )
+        child: Text(
+          "Hello World",
+          style: TextStyle(fontSize: 20),
         ),
+      )),
     );
   }
 }
