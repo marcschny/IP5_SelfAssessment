2be15f89a941e2e98c225909a0ea938691a22d34
Schnydrig Marc (s) <marc.schnydrig@students.fhnw.ch>
2020-11-16 20:18:24 +0100

    set up start screen

--- lib/screens/0_Start.dart
+++ lib/screens/0_Start.dart
@@ -14,66 +14,131 @@ class StartScreen extends StatelessWidget{
     return Scaffold(
       body: Stack(
         children: [
-          Container(
-            color: Colors.transparent,
+          //custom curved shape (fills entire screen)
+          Positioned(
+            top: 0,
+            left: 0,
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height,
-            child: CustomPaint(
-              painter: CurvedShape(ThemeColors.greenShade1, ThemeColors.greenShade3),
+            child: Container(
+              color: Colors.transparent,
+              child: CustomPaint(
+                painter: CurvedShape(ThemeColors.greenShade1, ThemeColors.greenShade3),
+              ),
             ),
           ),
 
-          Container(
+          //assessment titles
+          Positioned(
+            top: 0,
+            left: 0,
             width: MediaQuery.of(context).size.width,
             height: MediaQuery.of(context).size.height*0.25,
-            padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
-            child: Center(
-              child: RichText(
-                maxLines: 2,
-                overflow: TextOverflow.clip,
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                  children: [
-                    TextSpan(
-                      text: "Kurz Assessment\n",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w500,
-                        color: Colors.black,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+              child: Center(
+                child: RichText(
+                  maxLines: 2,
+                  overflow: TextOverflow.clip,
+                  textAlign: TextAlign.center,
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Kurz Assessment\n",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.black,
+                        ),
                       ),
-                    ),
-                    TextSpan(
-                      text: "Soziale Beziehungen",
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.black
+                      TextSpan(
+                        text: "Soziale Beziehungen",
+                        style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
           ),
 
 
-          /*Container(
-            margin: EdgeInsets.only(top: MediaQuery.of(context).size.height*0.3),
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 12),
-            color: Colors.red,
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                children: [
-                  TextSpan(
-                    text: "Kontakte mit anderen Menschen sind für uns alle lebenswichtig.",
-                    style: TextStyle(color: Colors.black),
-                  ),
-                ],
-              ),
+          //intro texts
+          Positioned(
+            top: MediaQuery.of(context).size.height*0.25+20,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: RichText(
+                textAlign: TextAlign.center,
+                text: TextSpan(
+                  children: [
+                    TextSpan(
+                      text: "Kontakte mit anderen Menschen sind für uns alle lebenswichtig.\n\n",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17
+                      ),
+                    ),
+                    TextSpan(
+                      text: "Hier kannst Du zunächst aufzeichnen, wer für Dich in Deinem Leben wichtig ist (das nennt sich: eine Netzwerkkarte zeichnen).\n\n",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17
+                      ),
+                    ),
+                    TextSpan(
+                      text: " Danach kannst Du herausfinden, was Du bereits sehr gut kannst im Umgang mit anderen Menschen und wo Du selber noch nicht so zufrieden bist mit Dir. \n\n",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17
+                      ),
+                    ),
+                    TextSpan(
+                      text: " Und dann startest Du Dein Veränderungsprojekt «Hey, das kann ich!»",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17
+                      ),
+                    ),
+                  ],
+                ),
 
+              ),
             ),
-          ),*/
+          ),
+
+
+          //start button
+          Positioned(
+            bottom: 40,
+            width: MediaQuery.of(context).size.width,
+            height: 60,
+            child: Center(
+              child: RaisedButton(
+                color: ThemeColors.greenShade2,
+                padding: EdgeInsets.symmetric(vertical: 11, horizontal: 34),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(40)
+                ),
+                elevation: 5.0,
+                child: Text(
+                  "Starten",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                    letterSpacing: 0.8,
+                  ),
+                ),
+                onPressed: (){},
+              ),
+            ),
+          )
+
 
         ],
       ),
